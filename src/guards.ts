9e71d525633ef4e17752fd9cb73734fe6b4b60e1
swordsgay <swordsgay@nonsense.garden>
2019-11-12 05:19:29 +0000

    Increase complexity!

--- src/guards.ts
+++ src/guards.ts
@@ -1,5 +1,53 @@
-let GuardTypes: PointForwardGuard | ForwardWeightedGuard | BackWeightedGuard;
-export {GuardTypes};
+
+export type FW = 'front-weighted';
+export type BW = 'back-weighted';
+
+export type Weightedness = FW | BW;
+
+export type R = 'dominant';
+export type L = 'offside';
+export type C = 'center'
+
+export type Sidedness = R | L | C;
+
+export type HI = 'high-line';
+export type LO = 'low-line';
+export type MID = 'middle';
+export type Level = HI | LO | MID;
+
+export type PointPosition = 'on-line' | 'off-line' | 'di-donna';
+
+export class Poste {
+    constructor(readonly weight: Weightedness,
+                readonly side: Sidedness, 
+                readonly level: Level,
+                readonly pointpos: PointPosition)
+                {Poste.all_poste.push(this);};
+    static all_poste: Poste[] = [];
+}
+
+export class PosteDiDonna extends Poste {
+    constructor(weight: Weightedness, side: Sidedness){
+        super(weight, side, 'high-line', 'di-donna');
+    }
+}
+
+export class BackweightedPosteDiDonna extends PosteDiDonna {
+    constructor(side: Sidedness){super('back-weighted', side);}
+}
+
+export let DominantSidePosteLonge = new Poste('front-weighted', 'dominant', 'middle', 'on-line');
+
+export let PosteFrontale = new Poste('front-weighted','center', 'high-line', 'on-line');
+
+export let DominantBackweightedPosteDiDonna = new BackweightedPosteDiDonna('dominant');
+
+
+export let PosteTuttePortaDiFerro = new Poste(
+    'front-weighted',
+     'dominant',
+      'low-line',
+      'off-line');
 
 export interface Guard{
     name: string;
@@ -17,9 +65,3 @@ export interface BackWeightedGuard extends Guard{
 }
 
 export interface DominantSideGuard extends Guard{}
-
-export let BackWeightedDominantSidePostaDiDonna: BackWeightedGuard & DominantSideGuard = {
-    name: "Dominant Posta Di Donna", 
-    shiftWeight() {console.log("weight shifted");}
-}
-
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,29 @@
-import { sayHello } from './greet';
-
 import * as guards from './guards';
+import {sample} from 'underscore';
 
 function showState(divName: string, fighter1: Fighter, fighter2:Fighter) {
     const elt = document.getElementById(divName);
-    elt.innerText = fighter1.name + " is fighting " + fighter2.name;
+
+    elt.innerText += guards.Poste.all_poste.length
+    elt.innerText += fighter1.name + " is fighting " + fighter2.name + "\n";
+    let poste: guards.Poste = fighter1.currentGuard;
+    elt.innerText += poste.weight;
+    
+    elt.innerText += fighter2.currentGuard.level;
 }
 
 class Fighter {
     constructor(readonly name: string)
-        {this.currentGuard = guards.BackWeightedDominantSidePostaDiDonna;};
-    currentGuard: guards.Guard;
+        {
+            this.legalPoste = guards.Poste.all_poste;
+            this.currentGuard = sample(this.legalPoste);
+        };
+    currentGuard: guards.Poste;
+    legalPoste: guards.Poste[];
 }
 
 let Alice = new Fighter('Alice');
 let Bob = new Fighter ('Bob');
 
 showState('greeting', Alice, Bob);
+console.log(guards.Poste.all_poste)
